089367439d8e45367fbeb7fd4a1471a460f9a89c
kawlik <kpawlik1103@gmail.com>
2022-05-31 10:26:58 +0200

    Adding client and store public folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { PrismaModule } from './prisma/prisma.module';
 import { WydawnictwoModule } from './wydawnictwo/wydawnictwo.module';
 import { KsiazkaModule } from './ksiazka/ksiazka.module';
 import { GatunekLiterackiModule } from './gatunek_literacki/gatunek_literacki.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-    imports: [AutorModule, PrismaModule, WydawnictwoModule, KsiazkaModule, GatunekLiterackiModule],
+    imports: [
+
+        //  local modules
+        AutorModule,
+        PrismaModule,
+        WydawnictwoModule,
+        KsiazkaModule,
+        GatunekLiterackiModule,
+
+        //  utilities
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public'),
+        }),
+    ],
     controllers: [],
     providers: [],
 })
